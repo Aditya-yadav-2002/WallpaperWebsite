a5c60164bc40a4333282c3efd1f0c0192735a27d
Aditya <adityaramhariram@gmail.com>
2021-08-29 20:33:42 +0530

    my website

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 80;
+
+// *******use*********
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
+
+// ********set**********
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// ********get******
+app.get("/", (req, res) => {
+  res.status(200).render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about.ejs");
+});
+
+app.get("/page3", (req, res) => {
+  res.status(200).render("page3.ejs");
+});
+
+app.get("/page4", (req, res) => {
+  res.status(200).render("page4.ejs");
+});
+
+app.get("/page5", (req, res) => {
+  res.status(200).render("page5.ejs");
+});
+
+// ********lisitenOnPort********
+app.listen(port,()=>{
+    console.log(`app is listen on post ${port}`);
+})
