e6492feec4d0628c7f7aefd17b8ddf28a41d270f
Aditya <adityaramhariram@gmail.com>
2021-08-29 20:53:07 +0530

    again modified

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 80;
+const port = process.env.PORT || 8000;
 
 // *******use*********
 app.use("/static", express.static("static"));
